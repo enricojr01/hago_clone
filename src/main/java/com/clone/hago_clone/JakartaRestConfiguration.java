5d2c467ddce8f8a5cf035ca0df2321d4306c7925
Enrico Jr Tuvera <5615794+enricojr01@users.noreply.github.com>
2026-03-19 11:42:41 +0800

    first commit

---
+++ src/main/java/com/clone/hago_clone/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.clone.hago_clone;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
