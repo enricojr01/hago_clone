5455769817392c32eebda6f4a37d9880c3f8d2d2
Anonymous <N/A>
2026-04-09 09:33:08 +0800

    fixed CreateAdminUser.java

--- src/main/java/com/clone/hago_clone/CreateAdminUser.java
+++ src/main/java/com/clone/hago_clone/CreateAdminUser.java
@@ -4,7 +4,9 @@
  */
 package com.clone.hago_clone;
 
+import com.clone.hago_clone.db.ClinicDAO;
 import com.clone.hago_clone.db.EmployeeDAO;
+import com.clone.hago_clone.models.ClinicBean;
 import java.sql.SQLException;
 
 /**
@@ -13,33 +15,41 @@ import java.sql.SQLException;
  */
 public class CreateAdminUser {
 
-	public static EmployeeDAO createDAO() {
-		try {
-			return new EmployeeDAO(
-					"jdbc:mysql://localhost:3306/javaclass",
-					"root",
-					""
-			);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return null;
-	}
+    private static final String url = "jdbc:mysql://localhost:3306/javaclass";
+    private static final String uname = "root";
+    private static final String pword = "";
 
-	public static void main(String[] args) {
-		try {
-			EmployeeDAO ed = createDAO();
-			ed.addEmployee(
-					"superadmin", 
-					"sadmin", 
-					"sadmin", 
-					"123456"
-			);
-			System.err.println("Created new admin user");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+    public static EmployeeDAO createDAO() {
+        try {
+            return new EmployeeDAO(
+                    "jdbc:mysql://localhost:3306/javaclass",
+                    "root",
+                    ""
+            );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        try {
+            EmployeeDAO ed = new EmployeeDAO(url, uname, pword);
+            ClinicDAO cd = new ClinicDAO(url, uname, pword);
+            
+            cd.createClinicTable();
+            ed.createEmployeeTable();
+            
+            ed.addEmployee("superadmin", "sadmin", "sadmin", "123456");
+            
+            System.out.println("Created new admin user");            
+        } catch (ClassNotFoundException e) {            
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
