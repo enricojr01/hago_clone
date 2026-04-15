7fe33bfb0380bf3569d98134ec3221393f85bcc2
Anonymous <N/A>
2026-04-15 11:42:45 +0800

    merged main into frontend preemptively, also created the PatientLoginServlet

---
+++ src/main/java/com/clone/hago_clone/servlets/PatientLoginServlet.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.clone.hago_clone.servlets;
+
+import com.clone.hago_clone.db.PatientDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author anonymous
+ */
+@WebServlet(name = "PatientLoginServlet", urlPatterns = {"/patientLogin"})
+public class PatientLoginServlet extends HttpServlet {
+    private PatientDAO db;
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * Get the login page if we are not logged in, or if we are then we skip to the dashboard as is
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * Validates the login page form. If successful, then go to the dashboard, else return back to the login page with the error info
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
